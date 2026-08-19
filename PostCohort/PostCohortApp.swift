3b047ec41f8e8510dfdfb7092f3df3784d902757
gaylelemuel.bit <gaylelemuel@gmail.com>
2026-08-18 19:37:54 -0500

    Initial Commit

---
+++ PostCohort/PostCohortApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostCohortApp.swift
+//  PostCohort
+//
+//  Created by Lemuel Gayle on 8/18/26.
+//
+
+import SwiftUI
+
+@main
+struct PostCohortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
